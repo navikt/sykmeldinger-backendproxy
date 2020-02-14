e279ee2cf5d30c81254aa2dc26a54a492880cb53
Joakim Kartveit <joakimkartveit@gmail.com>
2020-02-14 11:54:09 +0100

    Deployer med riktig api key

--- launch.sh
+++ launch.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-#export SERVICE_GATEWAY_KEY= $(</secrets/apikey/x-nav-apiKey)
-#export SERVICE_GATEWAY_URL= 'https'
-export SERVICE_GATEWAY_KEY='https'
+export SERVICE_GATEWAY_KEY=$(</secrets/apikey/x-nav-apiKey)
 envsubst < /tmp/default.conf.template > /etc/nginx/conf.d/default.conf '$SERVICE_GATEWAY_URL $SERVICE_GATEWAY_KEY $ALLOW_ORIGIN'
 
 exec nginx -g 'daemon off;'
