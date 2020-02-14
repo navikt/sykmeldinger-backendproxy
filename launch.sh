6a5039275eab37a96f04937d337918d8beebb840
Joakim Kartveit <joakimkartveit@gmail.com>
2020-02-14 11:49:17 +0100

    dropper space

--- launch.sh
+++ launch.sh
@@ -2,6 +2,7 @@
 
 #export SERVICE_GATEWAY_KEY= $(</secrets/apikey/x-nav-apiKey)
 #export SERVICE_GATEWAY_URL= 'https'
+export SERVICE_GATEWAY_KEY='https'
 envsubst < /tmp/default.conf.template > /etc/nginx/conf.d/default.conf '$SERVICE_GATEWAY_URL $SERVICE_GATEWAY_KEY $ALLOW_ORIGIN'
 
 exec nginx -g 'daemon off;'
