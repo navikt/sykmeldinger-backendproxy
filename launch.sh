ec822172cac8494d48a15c7716337886bed155dc
Joakim Kartveit <joakimkartveit@gmail.com>
2020-02-14 10:05:23 +0100

    Prøver å lese ut apikey og legge til SERVICE_GATEWAY_URL

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source /var/run/secrets/nais.io/vault/credentials.env
+export SERVICE_GATEWAY_KEY= $(</secrets/apikey/x-nav-apiKey)
 envsubst < /tmp/default.conf.template > /etc/nginx/conf.d/default.conf '$SERVICE_GATEWAY_URL $SERVICE_GATEWAY_KEY $ALLOW_ORIGIN'
 
 exec nginx -g 'daemon off;'
