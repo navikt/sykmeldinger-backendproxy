1bc142cf27f00fdf334d7d4bcedaada7fb8f55ab
Joakim Kartveit <joakimkartveit@gmail.com>
2020-02-14 10:31:44 +0100

    Legger til dumm SERVICE_GATEWAY_URL

--- launch.sh
+++ launch.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export SERVICE_GATEWAY_KEY= $(</secrets/apikey/x-nav-apiKey)
+export SERVICE_GATEWAY_URL= "https"
 envsubst < /tmp/default.conf.template > /etc/nginx/conf.d/default.conf '$SERVICE_GATEWAY_URL $SERVICE_GATEWAY_KEY $ALLOW_ORIGIN'
 
 exec nginx -g 'daemon off;'
