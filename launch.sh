12501e5f579506ab35bb2d172191e5e2097e33ea
andreasnilsen <danduras@gmail.com>
2020-06-24 13:06:53 +0200

    Update launch.sh source credentials.env

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SERVICE_GATEWAY_KEY=$(</secrets/apikey/x-nav-apiKey)
+source /var/run/secrets/nais.io/vault/credentials.env
 envsubst < /tmp/default.conf.template > /etc/nginx/conf.d/default.conf '$SERVICE_GATEWAY_URL $SERVICE_GATEWAY_KEY $ALLOW_ORIGIN'
 
 exec nginx -g 'daemon off;'
